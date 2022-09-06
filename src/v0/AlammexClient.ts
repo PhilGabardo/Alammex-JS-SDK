e72a0e281710e26b0b0b7b728f156860528bd904
Phil Gabardo <gabardpj@gmail.com>
2022-09-06 14:09:44 -0700

    AlammexClient: support disabledProtocols and maxGroupSize arguments for fetchQuote

--- src/v0/AlammexClient.ts
+++ src/v0/AlammexClient.ts
@@ -26,7 +26,7 @@ export default class AlammexClient {
 		return new this(algodUri, algodToken, algodPort, 'testnet', apiKey)
 	}
 
-	async getFixedInputSwapQuote(fromASAId, toASAId, amount) : Promise<AlammexQuote> {
+	async getFixedInputSwapQuote(fromASAId, toASAId, amount, disabledProtocols = [], maxGroupSize = 16) : Promise<AlammexQuote> {
 		const quote = await fetchApiData(this.chain, 'fetchQuote', {
 			algodUri: this.algodUri,
 			algodToken: this.algodToken,
@@ -35,6 +35,8 @@ export default class AlammexClient {
 			amount: amount,
 			fromASAID: fromASAId,
 			toASAID: toASAId,
+			disabledProtocols: disabledProtocols,
+			maxGroupSize: maxGroupSize
 		})
 		return AlammexQuote.fromAPIResponse(quote)
 	}
